4af29a65dc054ca339e549f0b3fbaeba2676a67b
jasoneperkins <webdev@jasoneperkins.com>
2026-01-30 16:14:17 -0500

    Updated sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,1 +1,34 @@
-C:/Users/jason/Documents/Development/astro/school/studio/sanity.config.ts
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { presentationTool } from 'sanity/presentation'
+
+import { schemaTypes } from './schemas/index'
+
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID || 'nn95ygkm'
+const dataset = process.env.PUBLIC_SANITY_DATASET || 'production'
+
+export default defineConfig({
+	name: 'temple-beit-hayam',
+	title: 'Temple Beit HaYam',
+	projectId: projectId,
+	dataset: dataset,
+	plugins: [
+		presentationTool({
+			previewUrl: {
+				origin: 'http://localhost:4321',
+				previewMode: {
+					enable: '/api/draft'
+				}
+			}
+		}),
+		structureTool()
+	],
+	schema: {
+		types: schemaTypes
+	},
+	useCdn: false,
+	stega: {
+		enabled: true,
+		studioUrl: 'https://localhost:4321'
+	}
+})
