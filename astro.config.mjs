11e98dafd389965a9ace776daac8226098c8ba33
jasoneperkins <webdev@jasoneperkins.com>
2026-02-01 18:27:02 -0500

    Initial commit of app rewrite.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,74 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config'
+import { loadEnv } from 'vite'
 
-// https://astro.build/config
-export default defineConfig({});
+import cloudflare from '@astrojs/cloudflare'
+import icon from 'astro-icon'
+import react from '@astrojs/react'
+import sanity from '@sanity/astro'
+import tailwindcss from '@tailwindcss/vite'
+
+const env = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), '')
+
+export default defineConfig({
+  adapter: cloudflare(),
+  env: {
+    schema: {
+      PUBLIC_SANITY_DATASET: envField.string({
+        context: 'client',
+        access: 'public',
+        default: 'production'
+      }),
+      PUBLIC_SANITY_PROJECT_ID: envField.string({
+        context: 'client',
+        access: 'public',
+        default: 'nn95ygkm'
+      }),
+      PUBLIC_SANITY_STUDIO_URL: envField.string({
+        context: 'client',
+        access: 'public',
+        default: 'http://localhost:3333'
+      }),
+      PUBLIC_SANITY_VISUAL_EDITING: envField.boolean({
+        context: 'client',
+        access: 'public',
+        default: false
+      }),
+      SANITY_API_TOKEN: envField.string({
+        context: 'server',
+        access: 'secret',
+        default: ''
+      }),
+      SANITY_PREVIEW_SECRET: envField.string({
+        context: 'server',
+        access: 'secret',
+        default: ''
+      })
+    }
+  },
+  integrations: [
+    icon({
+      include: {
+        'line-md': [
+          'chevron-small-down',
+          'email-filled',
+          'facebook',
+          'map-marker-twotone-loop',
+          'phone-twotone-loop',
+          'sunny-twotone-loop'
+        ],
+        lucide: ['baby', 'blocks', 'palette', 'graduation-cap']
+      }
+    }),
+    react(),
+    sanity({
+      projectId: env.PUBLIC_SANITY_PROJECT_ID,
+      dataset: env.PUBLIC_SANITY_DATASET,
+      useCdn: false
+    })
+  ],
+  output: 'server',
+  vite: {
+    plugins: [tailwindcss()]
+  }
+})
