294e53a9dbbb401f3d007d3c31b45ddfa52ee535
jasoneperkins <webdev@jasoneperkins.com>
2026-02-07 09:27:23 -0500

    refactor: rename /playground route to /studio

---
+++ studio/sanity.cli.ts
@@ -0,0 +1,13 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'nn95ygkm',
+    dataset: 'production',
+  },
+  deployment: {
+    appId: 'jmig666w24rvg0w32fafblkr',
+    autoUpdates: true,
+  },
+  studioHost: 'eclcstuart',
+})
